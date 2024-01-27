d88d3a39229fd00ecb235010b97e877516cee3fe
George Alexandre Fonseca Feitosa Junior <georgefeitosa@MacBook-Air.local>
2024-01-27 00:57:49 -0300

    Add CI workflow

---
+++ src/types/jest-styled-components.d.ts
@@ -0,0 +1,34 @@
+import { NewPlugin } from 'pretty-format'
+import { css } from 'styled-components'
+
+declare global {
+  namespace jest {
+    interface AsymmetricMatcher {
+      $$typeof: symbol
+      sample?: string | RegExp | object | Array
+    }
+
+    type Value = string | number | RegExp | AsymmetricMatcher | undefined
+
+    interface Options {
+      media?: string
+      modifier?: string | ReturnType<typeof css>
+      supports?: string
+    }
+
+    interface Matchers<R> {
+      toHaveStyleRule(property: string, value?: Value, options?: Options): R
+    }
+  }
+}
+
+export interface StyledComponentsSerializerOptions {
+  addStyles?: boolean
+  classNameFormatter?: (index: number) => string
+}
+
+export declare const styleSheetSerializer: NewPlugin & {
+  setStyleSheetSerializerOptions: (
+    options?: StyledComponentsSerializerOptions
+  ) => void
+}
